a7722aedbba56ffd464bc9f9dd01ccd8c42f10b2
Jasso <501286367@hdi.com.mx>
2022-02-15 08:46:42 -0600

    Falta actualizar request

--- app.mjs
+++ app.mjs
@@ -62,6 +62,7 @@ function insertOffice(index) {
             };
 
             const request = new Request(
+                // Actualizar Request !!!
                 "EXEC USP_BI_CSL_insert_reg_RegistroTemperaturaXidOdicina @IdOficina, @Humedad, @Nubes, @Sensacion, @Temperatura, @Descripcion",
                 function (err) {
                     if (err) {
@@ -72,11 +73,27 @@ function insertOffice(index) {
                 }
             );
             request.addParameter("IdOficina", TYPES.SmallInt, myObject.Id_Oficina);
-            request.addParameter("Humedad", TYPES.SmallInt, myObject.Humedad);
-            request.addParameter("Nubes", TYPES.SmallInt, myObject.Nubes);
-            request.addParameter("Sensacion", TYPES.Float, myObject.Sensacion);
-            request.addParameter("Temperatura", TYPES.Float, myObject.Temperatura);
+            request.addParameter("Latitud", TYPES.Float, myObject.Latitud);
+            request.addParameter("Longitud", TYPES.Float, myObject.Longitud);
+            request.addParameter("Id", TYPES.SmallInt, myObject.Id);
+            request.addParameter("Principal", TYPES.NVarChar, myObject.Principal);
             request.addParameter("Descripcion", TYPES.NVarChar, myObject.Descripcion);
+            request.addParameter("Icono", TYPES.SmallInt, myObject.Icono);
+            request.addParameter("Temperatura", TYPES.Float, myObject.Temperatura);
+            request.addParameter("Sensacion", TYPES.Float, myObject.Sensacion);
+            request.addParameter("Presion", TYPES.Float, myObject.Presion);
+            request.addParameter("Humedad", TYPES.Float, myObject.Humedad);
+            request.addParameter("Temperatura_minima", TYPES.Float, myObject.Temperatura_minima);
+            request.addParameter("Temperatura_maxima", TYPES.Float, myObject.Temperatura_maxima);
+            request.addParameter("Nivel_del_mar", TYPES.Float, myObject.Nivel_del_mar);
+            request.addParameter("Nivel_de_tierra", TYPES.Float, myObject.Nivel_de_tierra);
+            request.addParameter("Velocidad_del_viento", TYPES.Float, myObject.Velocidad_del_viento);
+            request.addParameter("Rafagas_de_viento", TYPES.Float, myObject.Rafagas_de_viento); //
+            request.addParameter("Nubes", TYPES.NVarChar, myObject.Nubes);
+            request.addParameter("Tiempo_del_calculo_del_clima", TYPES.Int, myObject.Tiempo_del_calculo_del_clima);
+            request.addParameter("Zona_horaria", TYPES.Int, myObject.Zona_horaria);
+            request.addParameter("ID_ciudad", TYPES.Int, myObject.ID_ciudad);
+            request.addParameter("Nombre_de_la_ciudad", TYPES.NVarChar, myObject.Nombre_de_la_ciudad);
 
             request.on("row", function (columns) {
                 columns.forEach(function (column) {
